05555a695dd8c16972fa764af0b3420e368fc594
zhaochangwu <changwuzhao@cienet.com.cn>
2016-08-11 12:19:20 +0800

    Init project

---
+++ ZCWCustomTools/ZCWCustomTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCWCustomTools
+//
+//  Created by zhaochangwu on 16/8/11.
+//  Copyright © 2016年 zhaochangwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCWCustomTools/ZCWCustomTools/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZCWCustomTools
+//
+//  Created by zhaochangwu on 16/8/11.
+//  Copyright © 2016年 zhaochangwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
