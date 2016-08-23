3066d450079a26de53f1fe1b7eca1c95fe61ab5b
zhaochangwu <changwuzhao@cienet.com.cn>
2016-08-23 09:38:45 +0800

    添加宏定义文件

---
+++ ZCWCustomTools/ZCWCustomTools/Class/ZCWDefine.h
@@ -0,0 +1,16 @@
+//
+//  ZCWDefine.h
+//  ZCWCustomTools
+//
+//  Created by zhaochangwu on 16/8/23.
+//  Copyright © 2016年 zhaochangwu. All rights reserved.
+//
+
+#ifndef ZCWDefine_h
+#define ZCWDefine_h
+
+#define kScreenBounds  [[UIScreen mainScreen] bounds]
+#define kScreenWidth   CGRectGetWidth(kScreenBounds)
+#define kScreenHeight  CGRectGetHeight(kScreenBounds)
+
+#endif /* ZCWDefine_h */
